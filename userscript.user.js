3d02dc40569cd3986413322cd7a0cd52be3cbf46
Myles Keller <43857093+myleskeller@users.noreply.github.com>
2021-01-15 10:40:06 -0500

    Create userscript.user.js

---
+++ userscript.user.js
@@ -0,0 +1,222 @@
+// ==UserScript==
+// @name         Overleaf Clarification
+// @version      0.1
+// @description  make sections and subsections more distinct (bolder) 
+// @author       Myles Keller
+// @match        https://*.overleaf.com/*
+// @grant        unsafeWindow
+// @icon         https://www.overleaf.com/favicon.ico
+// ==/UserScript==
+
+(function() {
+    "use strict";
+
+    //.rp-entry-comment
+    //document.querySelector("#review-panel > div.rp-entry-list.ng-scope > div > div:nth-child(1) > div > comment-entry > div > div.rp-entry.rp-entry-comment");
+    //document.querySelector("#review-panel > div.rp-entry-list.ng-scope > div");
+    let editorTarget = "#editor > div > div.ace-editor-body.ace_editor.ace_hidpi.ace-chrome > div.ace_scroller > div > div.ace_layer.ace_text-layer";
+    let reviewTarget = "#review-panel > div.rp-entry-list.ng-scope > div.rp-entry-wrapper.ng-scope";
+
+    waitForKeyElements (editorTarget, scriptCallbackFunction);
+
+    const scriptId = "overleaf-clarifier";
+    const commentSelector = ".rp-entry-comment";
+    const lineSelector = ".ace_type";
+
+    function log(...toLog) {
+        console.log(`[${scriptId}]:`, ...toLog);
+    }
+
+    function setAndGetNodeId(node) {
+        const nodeIdString = `${scriptId}-id`;
+
+        let nodeId = node.getAttribute(nodeIdString);
+        let hadNodeIdSet = true;
+
+        // log("new node found", { nodeId, hadNodeIdSet, node });
+
+        if (!nodeId) {
+            hadNodeIdSet = false;
+            nodeId = Math.random().toString();
+            node.setAttribute(nodeIdString, nodeId);
+        }
+
+        return { nodeId, hadNodeIdSet };
+    }
+
+    function addedNodeHandler(node) {
+        // log(`checked a node.`);
+
+        if (!node.matches || !node.matches(lineSelector)) {
+            // log(`a node matched!`);
+            return;
+        }
+
+        const { nodeId, hadNodeIdSet } = setAndGetNodeId(node);
+
+        if (!hadNodeIdSet) {
+            // this is a new element
+
+            // if it's a editor line /////////////////////
+            if (node.classList == "ace_storage ace_type"){
+
+                if (node.innerText.toUpperCase().trim() === "\\SUBSECTION") {
+                    //node.parentElement.style.backgroundColor = "lightgreen"; //this one works fine, but i can't use it without \Section...
+                    //node.childElement.style.backgroundColor = "#930f80";
+                    node.parentElement.style.fontWeight = "bolder";
+                    // node.style.fontWeight = "normal";
+                    node.parentElement.style.fontStyle = "italic";
+                    node.style.fontStyle = "normal";
+
+                    log(`changed style of ` + node.parentElement.className);
+                }
+
+                if (node.innerText.toUpperCase().trim() === "\\SECTION") {
+                    //node.parentElement.style.backgroundColor = "lightgreen"; //i cannot get this to work consistently...
+                    //node.childElement.style.backgroundColor = "#930f80";
+                    node.parentElement.style.fontWeight = "bolder";
+                    // node.style.fontWeight = "normal";
+
+                    log(`changed style of ` + node.parentElement.className);
+                }
+            }
+
+            // if it's a review comment ////////////////////////////
+            else if (node.classList == "rp-entry rp-entry-comment"){
+                var nodeText = node.querySelector("div.rp-comment-loaded > div > p > span > span:nth-child(2)").innerText;
+                if (nodeText.toUpperCase().startsWith("NOTE:")){
+                    node.style.borderColor = "blue";
+
+                    log(`changed style of ` + node.className);
+                }
+                if (nodeText.toUpperCase().startsWith("TODO:")){
+                    node.style.borderColor = "red";
+                    log(`changed style of ` + node.parentElement.className);
+                }
+            }
+        }
+    }
+
+    function scriptCallbackFunction (element) {
+        ////// for the editor ///////////////////////////////////////////////////
+        const editorBodyObserver = new MutationObserver(function(mutations) {
+            log(`checking editor with MutationObserver.`);
+
+            mutations.forEach(function(mutation) {
+                mutation.addedNodes.forEach(addedNode => {
+                    addedNodeHandler(addedNode);
+
+                    // it might be text node or comment node which don't have querySelectorAll
+                    addedNode.querySelectorAll &&
+                        addedNode.querySelectorAll(lineSelector).forEach(addedNodeHandler);
+                });
+            });
+        });
+
+
+        editorTarget = document.querySelector("#editor > div > div.ace-editor-body.ace_editor.ace_hidpi.ace-chrome > div.ace_scroller > div > div.ace_layer.ace_text-layer");
+
+        editorBodyObserver.observe(editorTarget, {
+            attributes: true,
+            childList: true,
+            subtree: true,
+            characterData: true
+        });
+/*
+        ////// for the review pane //////////////////////////////////////////////
+        const reviewBodyObserver = new MutationObserver(function(mutations) {
+            log(`checking review with MutationObserver.`);
+
+            mutations.forEach(function(mutation) {
+                mutation.addedNodes.forEach(addedNode => {
+                    addedNodeHandler(addedNode);
+
+                    // it might be text node or comment node which don't have querySelectorAll
+                    addedNode.querySelectorAll &&
+                        addedNode.querySelectorAll(commentSelector).forEach(addedNodeHandler);
+                });
+            });
+        });
+
+
+        reviewTarget = document.querySelector("#review-panel > div.rp-entry-list.ng-scope > div.rp-entry-wrapper.ng-scope > div > comment-entry > div > div.rp-entry.rp-entry-comment");
+        //reviewTarget = document.querySelector("div > comment-entry > div > div.rp-entry.rp-entry-comment");
+
+
+        reviewBodyObserver.observe(reviewTarget, {
+            attributes: true,
+            childList: true,
+            subtree: true,
+            characterData: true
+        });
+        */
+    }
+
+    //hacky way to wait for the fucking page to actually load.
+    function waitForKeyElements (
+    selectorTxt, /* Required: The selector string that
+                        specifies the desired element(s).
+                    */
+     actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+     bWaitOnce /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    ) {
+        var editorTargetNodes, beditorTargetsFound;
+        editorTargetNodes = document.querySelectorAll(selectorTxt);
+
+        if (editorTargetNodes && editorTargetNodes.length > 0) {
+            beditorTargetsFound = true;
+            /*--- Found editorTarget node(s).  Go through each and act if they
+            are new.
+        */
+            editorTargetNodes.forEach(function(element) {
+                var alreadyFound = element.dataset.found == 'alreadyFound' ? 'alreadyFound' : false;
+
+                if (!alreadyFound) {
+                    //--- Call the payload function.
+                    var cancelFound = actionFunction (element);
+                    if (cancelFound){
+                        beditorTargetsFound = false;
+                    }
+                    else{
+                        element.dataset.found = 'alreadyFound';}
+                }
+            } );
+        }
+        else {
+            beditorTargetsFound = false;
+        }
+
+        //--- Get the timer-control variable for this selector.
+        var controlObj = waitForKeyElements.controlObj || {};
+        var controlKey = selectorTxt.replace (/[^\w]/g, "_");
+        var timeControl = controlObj [controlKey];
+
+        //--- Now set or clear the timer as appropriate.
+        if (beditorTargetsFound && bWaitOnce && timeControl) {
+            //--- The only condition where we need to clear the timer.
+            clearInterval (timeControl);
+            delete controlObj [controlKey];
+        }
+        else {
+            //--- Set a timer, if needed.
+            if ( ! timeControl) {
+                timeControl = setInterval ( function () {
+                    waitForKeyElements (selectorTxt,
+                                        actionFunction,
+                                        bWaitOnce
+                                       );
+                },
+                                           300
+                                          );
+                controlObj [controlKey] = timeControl;
+            }
+        }
+        waitForKeyElements.controlObj = controlObj;
+    }
+})();
